d78192bb94d608b8f742f5baa3fd3ed29735e6d0
James Cole <jcole099@hotmail.com>
2022-11-22 14:07:13 -0800

    true rando complete

--- controllers/pickcancelled.js
+++ controllers/pickcancelled.js
@@ -41,10 +41,39 @@ module.exports = {
 
 			//TODO: this IF condition is just for testing, remove it BEFORE DEPLOYMENT
 			if (player.playerName === 'dmc_run') {
+				//BUILD POOL of available bets
+				//###########################################################################
+				let betsAvailable = JSON.parse(JSON.stringify(linesData));
+
+				//delete fighter lines
+				let opponentNames = [];
+				for (let playerBet of player.activeBets) {
+					playerBet = JSON.parse(playerBet);
+					for (let index in betsAvailable) {
+						if (playerBet.fighterName == betsAvailable[index].fighterName) {
+							opponentNames.push(betsAvailable[index].opponentName);
+							//deletes fighterLine
+							betsAvailable.splice(index, 1);
+							break;
+						}
+					}
+				}
+				//remove opponent lines
+				for (let opponentName of opponentNames) {
+					for (let i = betsAvailable.length - 1; i >= 0; i--) {
+						if (betsAvailable[i].fighterName === opponentName) {
+							betsAvailable.splice(i, 1);
+							break;
+						}
+					}
+				}
+				//###########################################################################
+
 				//iterate through each bet
 				for (let i = player.activeBets.length - 1; i >= 0; i--) {
 					let bet = JSON.parse(player.activeBets[i]);
 					let betAmount = 0;
+
 					//look for cancelled fight
 					if (!fighterNames.includes(bet.fighterName)) {
 						//fight has been cancelled...
@@ -58,15 +87,46 @@ module.exports = {
 						};
 						// delete current bet
 						player.activeBets.splice(i, 1);
-						//randomly select another fight to bet on
-						let randLineIndex = Math.floor(Math.random() * linesData.length);
 
-						//build bet object from randomly selected line
-						betTemplate.fighterName = linesData[randLineIndex].fighterName;
-						betTemplate.amountBet = betAmount; //use same bet amount
-						betTemplate.fighterOdds = linesData[randLineIndex].fighterOdds;
-						betTemplate = JSON.stringify(betTemplate);
-						player.activeBets.push(betTemplate);
+						//IF THERE ARE UNIQUE BETS AVAILABLE...
+						if (betsAvailable.length > 0) {
+							let randLineIndex = Math.floor(
+								Math.random() * betsAvailable.length
+							);
+							let opponentName = betsAvailable[randLineIndex].opponentName;
+
+							//build bet object from randomly selected line
+							betTemplate.fighterName =
+								betsAvailable[randLineIndex].fighterName;
+							betTemplate.amountBet = betAmount; //use same bet amount
+							betTemplate.fighterOdds =
+								betsAvailable[randLineIndex].fighterOdds;
+							betTemplate = JSON.stringify(betTemplate);
+							player.activeBets.push(betTemplate);
+
+							//delete betting line
+							betsAvailable.splice(randLineIndex, 1);
+
+							//delete opponentline
+							for (let i = betsAvailable.length - 1; i >= 0; i--) {
+								if (betsAvailable[i].opponentName === opponentName) {
+									betsAvailable.splice(i, 1);
+									break;
+								}
+							}
+						} else {
+							//THERE ARE NO UNIQUE BETS TO BET ON
+							//will either dup bet, or bet on opponent
+							//randomly select another fight to bet on
+							let randLineIndex = Math.floor(Math.random() * linesData.length);
+
+							//build bet object from randomly selected line
+							betTemplate.fighterName = linesData[randLineIndex].fighterName;
+							betTemplate.amountBet = betAmount; //use same bet amount
+							betTemplate.fighterOdds = linesData[randLineIndex].fighterOdds;
+							betTemplate = JSON.stringify(betTemplate);
+							player.activeBets.push(betTemplate);
+						}
 
 						//sort bets... probably pointless
 						player.activeBets = sortActiveBetsHelper(player.activeBets);
