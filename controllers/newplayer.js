4476622c4f700e908a0838e4796d2ce6d6867f1b
James Cole <jcole099@hotmail.com>
2022-12-01 18:57:33 +0000

    newplayer has an official welcome message

--- controllers/newplayer.js
+++ controllers/newplayer.js
@@ -69,7 +69,9 @@ module.exports = {
 			await Players.create(newPlayer);
 
 			const someGuy = await myGuild.members.fetch(message.author.id);
-			someGuy.send('Welcome to DFL! Placeholder text from !newplayer');
+			someGuy.send(
+				`Welcome to Discord Fight League, ${message.author.username}! Please visit our website, https://www.discordfightleague.com/howtoplay, for more information on how to start competing!`
+			);
 			return await warRoom.send(
 				`Welcome to Discord Fight League <@${message.author.id}>`
 			);
